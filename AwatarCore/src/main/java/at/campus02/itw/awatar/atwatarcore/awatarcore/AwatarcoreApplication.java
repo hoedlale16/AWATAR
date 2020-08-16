fb89df305dd15fccd9eeb004687f106b0a44677a
Alexander H <alexander.hoedl@edu.fh-joanneum.at>
2020-08-16 11:13:27 +0200

    initial creation of submodule AWATARcore

---
+++ AwatarCore/src/main/java/at/campus02/itw/awatar/atwatarcore/awatarcore/AwatarcoreApplication.java
@@ -0,0 +1,13 @@
+package at.campus02.itw.awatar.atwatarcore.awatarcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwatarcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwatarcoreApplication.class, args);
+    }
+
+}
---
+++ AwatarCore/src/test/java/at/campus02/itw/awatar/atwatarcore/awatarcore/AwatarcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package at.campus02.itw.awatar.atwatarcore.awatarcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwatarcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
